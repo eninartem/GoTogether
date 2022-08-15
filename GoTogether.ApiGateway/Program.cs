46c62c25283c9968aac4350f4331c38f3419a728
Enin Artem <artem.enin@onlyoffice.com>
2022-08-15 11:26:38 +0400

    init Ocelot gateway

--- GoTogether.ApiGateway/Program.cs
+++ GoTogether.ApiGateway/Program.cs
@@ -1,6 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration((hostContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+
+builder.Host.ConfigureLogging((hostContext, loggingBuilder) =>
+{
+    loggingBuilder.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
+
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
 
 app.Run();
